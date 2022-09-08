0ab6f7921a0001318903a54a033644cabdb75397
TeodorCholakov <teodorcholakov777@gmail.com>
2022-09-08 21:36:47 +0200

    changed dates Please enter the commit message for your changes. Lines starting

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function GetDate(){
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
 
-    document.getElementById('test').innerHTML = Date()
+  // display date
+  var dt = new Date();
+  document.getElementById('date-time').innerHTML=dt.toISOString();
 
-}
+
+  
